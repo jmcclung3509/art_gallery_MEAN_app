64a112a27bceba9933677de34981940a4a4069b9
Jessica Turner <jmcclung3509@gmail.com>
2022-08-31 11:55:45 -0700

    connected to localhost

--- src/app/add-page/add-page.component.ts
+++ src/app/add-page/add-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-
+HttpClient;
 @Component({
   selector: 'app-add-page',
   templateUrl: './add-page.component.html',
@@ -8,11 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPageComponent implements OnInit {
 
+superInfo!:any
 
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    formValues
+    this.http.post('http://localhost:3015/super', {formValues}).subscribe((res:any)=> {
+      this.superInfo = res?.data
+    })
   }
 
 }
--- src/app/view-page/view-page.component.ts
+++ src/app/view-page/view-page.component.ts
@@ -1,17 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
+HttpClient;
 @Component({
   selector: 'app-view-page',
   templateUrl: './view-page.component.html',
   styleUrls: ['./view-page.component.css']
 })
 export class ViewPageComponent implements OnInit {
-  @Input() superInfo: any;
-  
-
-  constructor() { }
+ superInfo!: any;
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.http.get('http://localhost:3015/super').subscribe((res:any)=> {
+      console.log(res)
+      this.superInfo = res?.data
+    })
   }
 
 }
